60667759d0ef8450e5935fe944a639707453c2b9
bertil291utn <bertilt4@gmail.com>
2020-05-08 18:50:31 -0500

    Create post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
     @new_post = Post.new
     @all_posts = Post.order(created_at: :desc).all
   end
+
+  def create
+    @new_post = Post.new(post_params)
+    if @new_post.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:comment)
+  end
 end
